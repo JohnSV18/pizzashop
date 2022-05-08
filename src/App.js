0a69736acb36cb3eeb2125300ffbe75c328d64d8
John Marcos <johnsaguay@gmail.com>
2022-05-08 13:08:02 -0700

    created pizza form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
+  const [name, setName] = useState("")
+  const [peperoni, setPeperoni] = useState(false)
+  const [sausage, setSausage] = useState(false)
+  const [onions, setOnions] = useState(false)
+  const [peppers, setPeppers] = useState(false)
+
+  return ( 
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h1>Pizza Order</h1>
+        <form className='PizzaForm'>
+        <input
+          type="text"
+          name='name'
+          placeholder='Name'
+          onChange={(e) => {setName(e.target.value)} }/>
+        <label>
+        <h3>Click on the toppings you would like to add!</h3>
+        <input 
+            type="checkbox"
+            checked={peperoni}
+            onChange={() => {setPeperoni(!peperoni)}}
+          />
+          Peperoni
+        </label>
+        <label>
+          <input 
+            type="checkbox"
+            checked={sausage}
+            onChange={() => {setSausage(!sausage)}}
+          />
+          Sausage
+        </label>
+        <label>
+        <input 
+            type="checkbox"
+            checked={peppers}
+            onChange={() => {setPeppers(!peppers)}}
+          />
+          Peppers
+        </label>
+        <label>
+        <input 
+            type="checkbox"
+            checked={onions}
+            onChange={() => {setOnions(!onions)}}
+          />
+          Onions
+        </label>
+        <input type="submit" value="Submit" />
+        </form>
+      </div>
+      <div className='DisplayPizza'>
+        <h2>{name}</h2>
+        {peperoni && <div>Pepperoni</div>}
+        {sausage && <div>Sausage</div>}
+        {onions && <div>Onions</div>}
+        {peppers && <div>Peppers</div>}
+      </div>
     </div>
   );
 }
